a956b2eb09d258384a7183364f7b4f8f024a75a0
Ashley <ashley_colleen@hotmail.com>
2020-08-10 15:56:10 -0400

    added URL prameters

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navigation from "./components/navigation/Navigation";
+import { Route } from "react-router-dom";
+import { render } from "@testing-library/react";
+// import { Switch } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Route
+          exact
+          path="/welcome/:name"
+          render={(props) => (
+            <Welcome {...props} name={props.match.params.name} />
+          )}
+        />
+
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    );
+  }
 }
-
 export default App;
