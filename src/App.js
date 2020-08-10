c9d9ca98b75afe03557c7e1059fb7a0302de4744
Ashley <ashley_colleen@hotmail.com>
2020-08-10 17:16:21 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -3,27 +3,33 @@ import "./App.css";
 import Welcome from "./components/welcome/Welcome";
 import Clock from "./components/clock/Clock";
 import Contact from "./components/contact/Contact";
-
+import Page404 from "./components/page404/Page404";
 import Navigation from "./components/navigation/Navigation";
-import { Route } from "react-router-dom";
-import { render } from "@testing-library/react";
-// import { Switch } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navigation />
-        <Route
-          exact
-          path="/welcome/:name"
-          render={(props) => (
-            <Welcome {...props} name={props.match.params.name} />
-          )}
-        />
+        <Switch>
+          <Route
+            path="/welcome/:name"
+            render={(props) => (
+              <Welcome {...props} name={props.match.params.name} />
+            )}
+          />
+          <Route
+            exact
+            path="/"
+            render={(props) => <Welcome {...props} name="Test" />}
+          />
 
-        <Route path="/clock" component={Clock} />
-        <Route path="/contact" component={Contact} />
+          <Route path="/clock" component={Clock} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Page404} />
+        </Switch>
       </div>
     );
   }
